8ae5c01f33defb008fc951197dd3c5675562e303
Whoisbezo.fx <bezzo19@gmx.de>
2025-11-30 23:15:39 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    {/* Leere Navbar */}
-    <nav className="navbar">
-      <div className="nav-wrapper">
-        {/* Keine Inhalte */}
-      </div>
-    </nav>
+        {/* Leere Navbar */}
+        <nav className="navbar">
+            <div className="nav-wrapper">
+                {/* Keine Inhalte */}
+            </div>
+        </nav>
     <\div>
   );
 }
