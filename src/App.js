4cf5ad2d4a4a58224af0c0c7e1385eb3d7e50445
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-01 21:41:00 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                 </div>
                 <div className="nav-right">
                     
-                    
+                    <SettingsButton />
                     <WalletConnect />
                 </div>
             </div>
