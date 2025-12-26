3616787710cf13b379b3b361518a294ef30f5f7c
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 05:40:59 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Login from "./Login";
 import './layout.css';
 
 function App() {
-  const [auth, setAuth] = useState(null);
-
-  useEffect(() => {
   return (
     <div className="App">
     </div>
