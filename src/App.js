22bf6100c557b0eb2feb06d92c1f1af6b06f858a
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 05:40:45 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -3,42 +3,12 @@ import Login from "./Login";
 
 import './layout.css';
 
-import WalletConnect from './components/WalletConnect';
-import SettingsButton from './components/SettingsButton';
-import SearchBar from './components/SearchBar';
-import logo from './assets/logo192.png';
-
 function App() {
   const [auth, setAuth] = useState(null);
 
   useEffect(() => {
-    fetch("/api/auth")
-      .then((res) => res.json())
-      .then((data) => setAuth(data.authenticated))
-      .catch(() => setAuth(false));
-  }, []);
-
-  // Warten, bis Auth geprüft wurde
-  if (auth === null) return null;
-
-  // Wenn nicht eingeloggt → Login anzeigen
-  if (!auth) return <Login />;
-
-  // Wenn eingeloggt → deine RendexFi App anzeigen
   return (
     <div className="App">
-      <nav className="navbar">
-        <div className="nav-wrapper">
-          <div className="nav-left">
-            <img src={logo} alt="Logo" className="logo" />
-            <SearchBar />
-          </div>
-          <div className="nav-right">
-            <SettingsButton />
-            <WalletConnect />
-          </div>
-        </div>
-      </nav>
     </div>
   );
 }
