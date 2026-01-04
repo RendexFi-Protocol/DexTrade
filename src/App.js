399f459e01e700a2b36af564422ff69a42d55f76
Whoisbezo.fx <bezzo19@gmx.de>
2026-01-05 00:15:50 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
-import Login from "./Login"; 
+import Login from "./Login";
 import "./layout.css";
 
 function App() {
+  const [auth, setAuth] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    fetch("/api/auth").then(res => {
+      if (res.ok) setAuth(true);
+      setChecked(true);
+    });
+  }, []);
+
+  if (!checked) return null;
+
   return (
     <div className="App">
-      <Login />   {
-        
-    }
+      {!auth && <Login />}
     </div>
   );
 }
